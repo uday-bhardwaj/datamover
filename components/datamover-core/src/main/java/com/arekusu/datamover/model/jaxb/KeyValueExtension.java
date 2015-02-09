668afc9eb728a80fdd23e4e79bff05e958351f12
Aleksandr Ananev <arrekusu.a@gmail.com>
2015-02-09 00:29:48 -0500

    Added StoreProcDAO

---
+++ components/datamover-core/src/main/java/com/arekusu/datamover/model/jaxb/KeyValueExtension.java
@@ -0,0 +1,71 @@
+
+package com.arekusu.datamover.model.jaxb;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{http://www.arekusu.com}KeyValueElement" maxOccurs="unbounded"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "keyValueElement"
+})
+@XmlRootElement(name = "KeyValueExtension", namespace = "http://www.arekusu.com")
+public class KeyValueExtension {
+
+    @XmlElement(name = "KeyValueElement", namespace = "http://www.arekusu.com", required = true)
+    protected List<KeyValueElement> keyValueElement;
+
+    /**
+     * Gets the value of the keyValueElement property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the keyValueElement property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getKeyValueElement().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link KeyValueElement }
+     * 
+     * 
+     */
+    public List<KeyValueElement> getKeyValueElement() {
+        if (keyValueElement == null) {
+            keyValueElement = new ArrayList<KeyValueElement>();
+        }
+        return this.keyValueElement;
+    }
+
+}
